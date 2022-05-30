7dbb12f0ed227e7928921c218ccd42f673242800
Carlos Bido <carloshbido@gmail.com>
2022-05-29 23:33:31 -0300

    removed urleconded

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 5000
 
 // Express format Data middlewares
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
 
 //Routes
 app.use('/login', require('./Routes/login'))
